43af7c1eed9416c7f8d3ce4daa110b9596c4bdf9
Suui <suui.gd@gmail.com>
2016-03-30 02:07:47 +0200

    Testing basics

---
+++ MongoSpike/Mongo.cs
@@ -0,0 +1,127 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MongoSpike
+{
+	public class Mongo
+	{
+		protected static IMongoClient Client { get; set; }
+		protected static IMongoDatabase Database { get; set; }
+
+		public Mongo()
+		{
+			Client = new MongoClient();
+			Database = Client.GetDatabase("test");
+		}
+
+		public async void InsertDocument()
+		{
+			var document = new BsonDocument
+			{
+				{"address", new BsonDocument
+					{
+						{"street", "2 Avenue"},
+						{"zipcode", "10075"},
+						{"building", "1480"},
+						{"coord", new BsonArray {73.9557413, 40.7720266}}
+					}
+				},
+				{"borough", "Manhattan"},
+				{"cuisine", "Italian"},
+				{"grades", new BsonArray
+					{
+						new BsonDocument
+						{
+							{"date", new DateTime(2014, 10, 1, 0, 0, 0, DateTimeKind.Utc)},
+							{"grade", "A"},
+							{"score", 11}
+						},
+						new BsonDocument
+						{
+							{"date", new DateTime(2014, 1, 6, 0, 0, 0, DateTimeKind.Utc)},
+							{"grade", "B"},
+							{"score", 17}
+						}
+					}
+				},
+				{"name", "Vella"},
+				{"restaurant_id", "41704620"}
+			};
+
+			var collection = Database.GetCollection<BsonDocument>("restaurants");
+			await collection.InsertOneAsync(document);
+		}
+
+		public async Task<int> TotalRestaurantDocuments()
+		{
+			var collection = Database.GetCollection<BsonDocument>("restaurants");
+			var filteredNone = new BsonDocument();
+			var count = 0;
+
+			var cursor = await collection.FindAsync(filteredNone);
+			while (await cursor.MoveNextAsync())
+			{
+				var batch = cursor.Current;
+				foreach (var document in batch)
+				{
+					count += 1;
+				}
+			}
+
+			return count;
+		}
+
+		public async Task<int> RestaurantsInManhattan()
+		{
+			var collection = Database.GetCollection<BsonDocument>("restaurants");
+			var filteredInManhattan = Builders<BsonDocument>.Filter.Eq("borough", "Manhattan");
+			var manhattanRestaurants = await collection.Find(filteredInManhattan).ToListAsync();
+
+			return manhattanRestaurants.Count;
+		}
+
+		public async Task<int> RestaurantsInManhattanLinq()
+		{
+			var collection = Database.GetCollection<BsonDocument>("restaurants");
+			var filteredNone = new BsonDocument();
+			var manhattanRestaurants = await collection.Find(filteredNone).ToListAsync();
+
+			return manhattanRestaurants.Where(restaurant => restaurant.GetValue("borough").Equals("Manhattan"))
+									   .ToList()
+									   .Count;
+		}
+
+		public async Task<int> RestaurantsWithZipcode10075()
+		{
+			var collection = Database.GetCollection<BsonDocument>("restaurants");
+			var filter = Builders<BsonDocument>.Filter.Eq("address.zipcode", "10075");
+			var restaurants = await collection.Find(filter).ToListAsync();
+
+			return restaurants.Count;
+		}
+
+		public async Task<int> RestaurantsWithZipcode10075Linq()
+		{
+			var collection = Database.GetCollection<BsonDocument>("restaurants");
+			var filteredNone = new BsonDocument();
+			var restaurants = await collection.Find(filteredNone).ToListAsync();
+
+			return restaurants.Where(restaurant => restaurant.GetValue("address").AsBsonDocument.GetValue("zipcode").Equals("10075"))
+							  .ToList()
+							  .Count;
+		}
+
+		public async Task<int> RestaurantsWithAScoreGreaterThan30()
+		{
+			var collection = Database.GetCollection<BsonDocument>("restaurants");
+			var filter = Builders<BsonDocument>.Filter.Gt("grades.score", 30);
+			var restaurants = await collection.Find(filter).ToListAsync();
+
+			return restaurants.Count;
+		}
+	}
+}
---
+++ MongoSpikeTest/MongoShould.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using MongoSpike;
+using NUnit.Framework;
+
+
+namespace MongoSpikeTest
+{	
+	[TestFixture]
+	class MongoShould
+	{
+		private Mongo Mongo { get; set; }
+
+		[SetUp]
+		public void GivenAMongoDatabase()
+		{
+			Mongo = new Mongo();
+		}
+
+		[Test]
+		public async Task have_25359_documents()
+		{
+			var totalRestaurantDocuments = await Mongo.TotalRestaurantDocuments();
+
+			totalRestaurantDocuments.Should().Be(25359);
+		}
+
+		[Test]
+		public async Task have_10259_restaurants_in_manhattan_via_driver_filter()
+		{
+			var restaurantsInManhattan = await Mongo.RestaurantsInManhattan();
+
+			restaurantsInManhattan.Should().Be(10259);
+		}
+
+		[Test]
+		public async Task have_10259_restaurants_in_manhattan_via_linq()
+		{
+			var restaurantsInManhattan = await Mongo.RestaurantsInManhattanLinq();
+
+			restaurantsInManhattan.Should().Be(10259);
+		}
+
+		[Test]
+		public async Task have_99_restaurants_with_zipcode_10075_via_driver_filter()
+		{
+			var restaurantsWithZipCode10075 = await Mongo.RestaurantsWithZipcode10075();
+
+			restaurantsWithZipCode10075.Should().Be(99);
+		}
+
+		[Test]
+		public async Task have_99_restaurants_with_zipcode_10075_via_linq()
+		{
+			var restaurantsWithZipcode10075 = await Mongo.RestaurantsWithZipcode10075Linq();
+
+			restaurantsWithZipcode10075.Should().Be(99);
+		}
+
+		[Test]
+		public async Task have_1959_restaurants_with_a_grade_score_greater_than_30()
+		{
+			var numberOfRestaurants = await Mongo.RestaurantsWithAScoreGreaterThan30();
+
+			numberOfRestaurants.Should().Be(1959);
+		}
+	}
+}
